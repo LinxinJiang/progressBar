4acda4609fa7935458f545a0fba5d170b906c7a8
Linxin Jiang <linxinjiang@LinxindeMacBook-Pro.local>
2023-04-21 14:11:00 -0400

    progressBar

---
+++ app/src/test/java/com/example/testprogressbar/MianActivityUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.testprogressbar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MianActivityUnitTest {
+
+
+    @Test
+    void progressBar_GoalsCounterIsNotZero(){
+        MainActivity mainActivity = new MainActivity();
+
+    }
+}
